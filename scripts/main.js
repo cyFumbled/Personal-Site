cc7200050eaf1cd3c8dfafd70b8069c2f119a3ea
Cyfumbled <161596440+cyFumbled@users.noreply.github.com>
2024-07-12 17:29:14 -0500

    Implemented dynamic animation delays through js
    Animation delay classes no longer need to be hardcoded into the stylesheet and are instead applied through parsing class names.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
+"use strict";
 
+
+const delayedElements = document.querySelectorAll("[class*='animation-delay']");
+
+for (let i = 0; i < delayedElements.length; i++) {
+    const delayduration = delayedElements[i].className.split(' ').find(element => element.startsWith('animation-delay')).split('-')[2];
+    delayedElements[i].style["animation-delay"] = delayduration;
+}
