d19734f86142597fac8002c632eda143d4c08f0a
Cyfumbled <161596440+cyFumbled@users.noreply.github.com>
2024-07-13 14:08:37 -0500

    Added Steam, Roblox, and Sendou.ink Accounts
    Added Steam, Roblox, and Sendou.ink Accounts to the Account directory.
    
    Implemented a dynamic background color and basic color styling scripts into main.js.

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,18 @@ for (let i = 0; i < delayedElements.length; i++) {
     const delayduration = delayedElements[i].className.split(' ').find(element => element.startsWith('animation-delay')).split('-')[2];
     delayedElements[i].style["animation-delay"] = delayduration;
 }
+
+
+const backgroundColoredElements = document.querySelectorAll("[class*='backgroundColor']");
+
+for (let i = 0; i < backgroundColoredElements.length; i++) {
+    const backgroundColor = backgroundColoredElements[i].className.split(' ').find(element => element.startsWith('backgroundColor')).split('-')[1];
+    delayedElements[i].style["background-color"] = backgroundColor;
+}
+
+const basicColoredElements = document.querySelectorAll("[class*='basicColor']");
+
+for (let i = 0; i <basicColoredElements.length; i++) {
+    const basicColor = basicColoredElements[i].className.split(' ').find(element => element.startsWith('basicColor')).split('-')[1];
+    basicColoredElements[i].style["color"] = basicColor;
+}
