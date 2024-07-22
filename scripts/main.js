a449c5462fcb41f9949075bc8ae5feee6043c729
Cyfumbled <161596440+cyFumbled@users.noreply.github.com>
2024-07-22 17:46:58 -0500

    Restyled competitive statistics to have collapsible elements

--- scripts/main.js
+++ scripts/main.js
@@ -22,3 +22,18 @@ for (let i = 0; i <basicColoredElements.length; i++) {
     const basicColor = basicColoredElements[i].className.split(' ').find(element => element.startsWith('basicColor')).split('-')[1];
     basicColoredElements[i].style["color"] = basicColor;
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
